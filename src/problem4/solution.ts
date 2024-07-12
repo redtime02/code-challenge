1bc0aebbe6c80700090e024dd10cca6c950c245f
redtime02 <phanthanhbinh089@gmail.com>
2024-07-12 17:52:07 +0700

    Solve problem 4

--- src/problem4/solution.ts
+++ src/problem4/solution.ts
@@ -1,21 +1,21 @@
 // Method 1: Using a for loop
-var sum_to_n_a1 = function (n) {
+function sum_to_n_a(n: number): number {
   let sum = 0;
   for (let i = 1; i <= n; i++) {
     sum += i;
   }
   return sum;
-};
+}
 
 // Method 2: Using the formula for the sum of the first n natural numbers
-var sum_to_n_b2 = function (n) {
+function sum_to_n_b(n: number): number {
   return (n * (n + 1)) / 2;
-};
+}
 
 // Method 3: Using recursion
-var sum_to_n_c3 = function (n) {
-  if (n === 1) {
-    return 1;
+function sum_to_n_c(n: number): number {
+  if (n <= 1) {
+    return n;
   }
-  return n + sum_to_n_c3(n - 1);
-};
+  return n + sum_to_n_c(n - 1);
+}
